a3b8b4d5d1d8246f24c584d93a5d1701b8a5b055
henry <henrybulingford@outlook.com>
2021-02-13 17:14:03 +0100

    New 3.0

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("new");
+        System.out.println("ok");
     }
 }
 
